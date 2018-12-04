d73198557bddc3115b4b2abc26733256b62ff11a
jimmyvogel <jvogel@excilys.com>
2018-12-04 19:16:30 +0100

    [all] start copying first lesson from projet from lyon web site

---
+++ IAInteractionist/src/main/java/com/mooc/ia/model/Interaction.java
@@ -0,0 +1,30 @@
+package com.mooc.ia.model;
+
+public interface Interaction {
+
+    /**
+     * @return The interaction's label
+     */
+    public String getLabel();
+
+    /**
+     * @return The interaction's experience
+     */
+    public Experiment getExperience();
+
+    /**
+     * @return The interaction's result
+     */
+    public Result getResult();
+
+    /**
+     * @param experience: The interaction's experience.
+     */
+    public void setExperience(Experiment experience);
+
+    /**
+     * @param result: The interaction's result.
+     */
+    public void setResult(Result result);
+
+}
---
+++ IAInteractionist/src/main/java/com/mooc/ia/model/Interaction010.java
@@ -0,0 +1,40 @@
+package com.mooc.ia.model;
+
+/**
+ * An interaction010 is the association of an experience with a result.
+ */
+public class Interaction010 implements Interaction {
+
+    private String label;
+    protected Experiment experience;
+    protected Result result;
+
+    public Interaction010(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return this.label;
+    }
+
+    public Experiment getExperience() {
+        return experience;
+    }
+
+    public void setExperience(Experiment experience) {
+        this.experience = experience;
+    }
+
+    public Result getResult() {
+        return result;
+    }
+
+    public void setResult(Result result) {
+        this.result = result;
+    }
+
+    public String toString() {
+        return this.experience.getLabel() + this.result.getLabel();
+    }
+
+}
